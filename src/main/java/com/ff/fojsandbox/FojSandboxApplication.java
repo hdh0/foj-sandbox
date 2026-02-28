c01c1c3670a1a767f0a91cc979cba286fdbe994c
hdh <hdh.h@qq.com>
2026-02-28 16:58:42 +0800

    Fix: 移除不必要的SandboxConfig依赖

--- src/main/java/com/ff/fojsandbox/FojSandboxApplication.java
+++ src/main/java/com/ff/fojsandbox/FojSandboxApplication.java
@@ -1,17 +1,10 @@
 package com.ff.fojsandbox;
 
-import com.ff.fojsandbox.config.SandboxConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.Resource;
-
 @SpringBootApplication
 public class FojSandboxApplication {
-
-    @Resource
-    private SandboxConfig sandboxConfig;
-
     public static void main(String[] args) {
         SpringApplication.run(FojSandboxApplication.class, args);
     }
